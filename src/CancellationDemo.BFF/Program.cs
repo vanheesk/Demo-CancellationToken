a344a726ad5f8e49162a04c6f583d6a7d022fcd1
Kristof Van Hees <2393120+vanheesk@users.noreply.github.com>
2024-12-12 16:55:13 +0100

    fix: Improvements on marking Cancellations as 'error'

--- src/CancellationDemo.BFF/Program.cs
+++ src/CancellationDemo.BFF/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.AspNetCore.Components.Web;
 using Yarp.ReverseProxy.Transforms;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +13,8 @@ builder.Services.AddSwaggerGen();
 
 // Add Application Insights
 builder.Services.AddApplicationInsightsTelemetry();
+builder.Services.AddSingleton<ITelemetryInitializer>(sp => new CustomTelemetryInitializer());
+builder.Services.AddSingleton<ITelemetryInitializer>(sp => new DependencyTelemetryInitializer());
 
 // Configure YARP
 builder.Services.AddReverseProxy()
@@ -23,14 +29,14 @@ builder.Services.AddReverseProxy()
                 context.HttpContext.Response.Headers.Add("X-Request-Cancelled", "true");
             }
         });
-        transforms.AddResponseTransform(async context =>
-        {
-            if (context.HttpContext.RequestAborted.IsCancellationRequested)
-            {
-                context.HttpContext.Response.StatusCode = 499;
-                context.HttpContext.Response.Headers.Add("X-Request-Cancelled", "true");
-            }
-        });
+        //transforms.AddResponseTransform(async context =>
+        //{
+        //    if (context.HttpContext.RequestAborted.IsCancellationRequested)
+        //    {
+        //        context.HttpContext.Response.StatusCode = 499;
+        //        context.HttpContext.Response.Headers.Add("X-Request-Cancelled", "true");
+        //    }
+        //});
     });
 
 var app = builder.Build();
@@ -47,3 +53,38 @@ app.MapReverseProxy();
 app.MapControllers();
 
 app.Run();
+
+public class CustomTelemetryInitializer : ITelemetryInitializer
+{
+    public void Initialize(ITelemetry telemetry)
+    {
+        if (telemetry is RequestTelemetry requestTelemetry)
+        {
+            if (requestTelemetry.ResponseCode == "499"
+                || requestTelemetry.ResponseCode == "Canceled")
+            {
+                requestTelemetry.Success = true;
+            }
+        }
+    }
+}
+
+public class DependencyTelemetryInitializer : ITelemetryInitializer
+{
+    public void Initialize(ITelemetry telemetry)
+    {
+        if (telemetry is DependencyTelemetry dependencyTelemetry)
+        {
+            if (dependencyTelemetry.ResultCode == "499" ||
+                dependencyTelemetry.ResultCode == "Canceled" ||
+                (dependencyTelemetry.Success == false 
+                    && dependencyTelemetry.Properties.ContainsKey("handledAt") 
+                    && dependencyTelemetry.Properties["handledAt"] == "OperationCanceledException")
+               )
+            {
+                dependencyTelemetry.Success = true;
+                dependencyTelemetry.Properties["handledAt"] = "OperationCanceledException";
+            }
+        }
+    }
+}
